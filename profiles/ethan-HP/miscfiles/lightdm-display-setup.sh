5b36ad69f7669d8e58122bd99a152bdfd458eb26
lufinkey <luisfinke@gmail.com>
2017-02-20 00:29:59 -0500

    turn off LVDS before VGA-0

--- profiles/ethan-HP/miscfiles/lightdm-display-setup.sh
+++ profiles/ethan-HP/miscfiles/lightdm-display-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ -n "$(xrandr | grep '^VGA-0 connected ')" ]
 then
-	xrandr --output VGA-0 --primary --auto
 	xrandr --output LVDS --off
+	xrandr --output VGA-0 --primary --auto
 fi
+
