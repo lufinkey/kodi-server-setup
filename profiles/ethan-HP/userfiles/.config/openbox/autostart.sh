aca7b5f67636c1d7e07955c5b262dfe6f6f2bc11
lufinkey <luisfinke@gmail.com>
2017-04-26 10:04:15 -0400

    disable dpms every login

--- profiles/ethan-HP/userfiles/.config/openbox/autostart.sh
+++ profiles/ethan-HP/userfiles/.config/openbox/autostart.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 xsetroot -solid '#000000'
 xrandr --output VGA-0 --primary --auto
+xset s off
+xset s noblank
+xset -dpms
--- user-setup.sh
+++ user-setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
-xset s off
-xset s noblank
-xset -dpms
-
