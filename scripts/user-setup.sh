9ee5e0aefb40e51b6f57151ffb0b86b659ba6f6e
lufinkey <luisfinke@gmail.com>
2017-10-04 19:24:48 -0400

    user setup for google chrome

--- scripts/user-setup.sh
+++ scripts/user-setup.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+if [ ! -f ~/.config/google-chrome/.kodi_setup_done ]
+then
+	echo "You need to configure Google Chrome. Please do the following:"
+	echo "    - sign into chrome"
+	echo "    - disable syncing"
+	echo "    - disable running in background (Settings > Advanced > System)"
+	echo "    - install Google Cast for Education extension"
+	echo "    - launch and configure Google Cast for Education"
+	echo "    - close chrome"
+	google-chrome "https://chrome.google.com/webstore/detail/google-cast-for-education/bnmgbcehmiinmmlmepibeeflglhbhlea?hl=en-US" &> /dev/null
+	touch ~/.config/google-chrome/.kodi_setup_done
+fi
+
