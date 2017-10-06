498a53706d3d1491f5a56105aba871f8f791a645
lufinkey <luisfinke@gmail.com>
2017-10-06 10:21:50 -0400

    create ROM folders in user setup

--- scripts/user-setup.sh
+++ scripts/user-setup.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
+
+# fix screen sleep
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.desktop.screensaver lock-enabled false
+
+# create rom folders
+mkdir -p ~/ROMs/Dreamcast
+mkdir -p ~/ROMs/Gameboy
+mkdir -p ~/ROMs/Gameboy\ Advance
+mkdir -p ~/ROMs/Gamecube
+mkdir -p ~/ROMs/Genesis
+mkdir -p ~/ROMs/NES
+mkdir -p ~/ROMs/Nintendo\ 64
+mkdir -p ~/ROMs/Nintendo\ DS
+mkdir -p ~/ROMs/Playstation
+mkdir -p ~/ROMs/Playstation\ 2
+mkdir -p ~/ROMs/SNES
+mkdir -p ~/ROMs/Wii
+
+# setup google chrome
 if [ ! -f ~/.config/google-chrome/.kodi_setup_done ]
 then
 	echo "You need to configure Google Chrome. Please do the following:"
