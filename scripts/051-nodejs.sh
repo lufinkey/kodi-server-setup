91485dc5f9dd72ff972fc018610c1caaad375718
lufinkey <luisfinke@gmail.com>
2017-12-19 03:23:40 -0500

    don't install npm packages if list is empty

--- scripts/051-nodejs.sh
+++ scripts/051-nodejs.sh
@@ -3,4 +3,9 @@
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 apt-get install -y nodejs build-essential
 
-npm install -g $(cat "data/lists/nodejs-packages.txt")
+npm_packages=$(cat "data/lists/nodejs-packages.txt")
+if [ -n "$npm_packages" ]
+then
+	npm install -g "$npm_packages"
+fi
+
