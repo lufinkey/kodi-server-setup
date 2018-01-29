bce4ac4e51de8d51930554ba1429bd2f7aec4d70
lufinkey <luisfinke@gmail.com>
2018-01-29 14:12:20 -0500

    make sure data/bin exists before entering

--- scripts/010-bin-files.sh
+++ scripts/010-bin-files.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd "data/bin"
-bin_files=(*)
-for bin_file in "${bin_files[@]}"
-do
-	cp -f "$bin_file" "/usr/local/bin/$bin_file"
-done
-
+if [ -d "data/bin" ]
+then
+	cd "data/bin"
+	bin_files=(*)
+	for bin_file in "${bin_files[@]}"
+	do
+		cp -f "$bin_file" "/usr/local/bin/$bin_file"
+	done
+fi
