5dad616fad27d0b33e122f5dc6f27578db088523
lufinkey <luisfinke@gmail.com>
2018-01-29 14:22:38 -0500

    update node version

--- scripts/051-nodejs.sh
+++ scripts/051-nodejs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | bash -
+# install nodejs
+curl -sL https://deb.nodesource.com/setup_9.x | bash -
 apt-get install -y nodejs build-essential
 
+# install npm packages
 npm_packages=$(cat "data/lists/nodejs-packages.txt")
 if [ -n "$npm_packages" ]
 then
 	npm install -g "$npm_packages"
 fi
-
